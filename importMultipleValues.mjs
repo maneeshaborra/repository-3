557ea055449595b6751f4ace2553f05f854491fd
maneeshaborra <maneeshakarinki1997@gmail.com>
2022-12-22 07:30:23 +0000

    Import and Export Multiple Values Solution

---
+++ importMultipleValues.mjs
@@ -0,0 +1,4 @@
+import { myArray } from "./exportMultipleValues.mjs";
+import { bulb } from "./exportMultipleValues.mjs";
+import { multiplyByFour } from "./exportMultipleValues.mjs";
+console.log(multiplyByFour(4));
